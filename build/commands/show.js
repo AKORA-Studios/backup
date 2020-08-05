e298dd8b22401f312cff2aa8c6a9e6b99bae167a
Maf <dragoncatm@gmail.com>
2020-08-05 22:35:35 +0200

    Bot can wait for File

--- build/commands/show.js
+++ build/commands/show.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const utilities_1 = require("../typescript/utilities");
 const classes_1 = require("../typescript/classes");
+const discord_js_1 = require("discord.js");
 //let a = new module();
 module.exports = new classes_1.Command({
     name: 'save',
@@ -13,7 +14,48 @@ module.exports = new classes_1.Command({
     user_permissions: ['ADMINISTRATOR'],
     bot_permissions: ['ADMINISTRATOR']
 }, async (msg, args) => {
-    var emb = utilities_1.newEmb(msg).setTitle("Exported Guild as JSON file").setColor(utilities_1.colors.success);
-    msg.channel.send(emb);
-    msg.channel.send(attachment);
+    //Getting the file from the User
+    var emb = utilities_1.newEmb(msg).setColor(utilities_1.colors.info);
+    emb.setTitle("Please send me your JSON File uwu").setDescription("*Write* `cancel` *to abort*").setFooter("I will wait 10 Seconds");
+    await msg.channel.send(emb);
+    var collector = msg.channel.createMessageCollector((m) => m.author.id == msg.author.id, {
+        time: 10000,
+    });
+    collector.on('collect', async (collected) => {
+        var m = collected.last();
+        //Canceling
+        if (m.content.toLowerCase().includes("cancel")) {
+            m.reply("Action canceled");
+            return collector.stop("Canceled");
+        }
+        //Check for Attachment
+        if (m.attachments.size < 1)
+            return m.reply("You need to send a File");
+        //Getting File
+        var attachment = m.attachments.first(), file = attachment.attachment, text = "";
+        if (file instanceof Buffer)
+            text = file.toString('utf8');
+        else if (typeof file == 'string')
+            text = file;
+        else
+            text = (await streamToString(file)) + "";
+        //Parsing
+        try {
+            var json = JSON.parse(text);
+            var buffer = Buffer.from(JSON.stringify(json, null, 4), 'utf8');
+            var attachment = new discord_js_1.MessageAttachment(buffer, 'backup.json');
+            msg.channel.send(attachment);
+        }
+        catch (err) {
+            return m.channel.send(utilities_1.newEmb(m).setColor(utilities_1.colors.error).setTitle("There was an error parsing your file ._."));
+        }
+    });
 });
+function streamToString(stream) {
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+        stream.on('data', chunk => chunks.push(chunk));
+        stream.on('error', reject);
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    });
+}
--- commands/show.ts
+++ commands/show.ts
@@ -1,6 +1,6 @@
 import { newEmb, importGuild, colors } from '../typescript/utilities';
 import { Command } from "../typescript/classes";
-import { MessageAttachment } from 'discord.js';
+import { MessageAttachment, Message, Collection } from 'discord.js';
 //let a = new module();
 
 module.exports = new Command({
@@ -15,10 +15,60 @@ module.exports = new Command({
 },
 
     async (msg, args) => {
-        var emb = newEmb(msg).setTitle("Exported Guild as JSON file").setColor(colors.success);
+        //Getting the file from the User
+        var emb = newEmb(msg).setColor(colors.info);
+        emb.setTitle("Please send me your JSON File uwu").setDescription("*Write* `cancel` *to abort*").setFooter("I will wait 10 Seconds");
+        await msg.channel.send(emb)
+
+        var collector = msg.channel.createMessageCollector(
+            (m: Message) => m.author.id == msg.author.id,
+            {
+                time: 10000,
+            }
+        );
+
+        collector.on('collect', async (collected: Collection<string, Message>) => {
+            var m = collected.last();
+
+            //Canceling
+            if (m.content.toLowerCase().includes("cancel")) {
+                m.reply("Action canceled")
+                return collector.stop("Canceled");
+            }
+
+            //Check for Attachment
+            if (m.attachments.size < 1) return m.reply("You need to send a File");
+
+            //Getting File
+            var attachment = m.attachments.first(),
+                file = attachment.attachment,
+                text = "";
+
+            if (file instanceof Buffer) text = file.toString('utf8');
+            else if (typeof file == 'string') text = file;
+            else text = (await streamToString(file)) + "";
+
+            //Parsing
+            try {
+                var json = JSON.parse(text);
 
 
-        msg.channel.send(emb);
-        msg.channel.send(attachment);
+                var buffer = Buffer.from(JSON.stringify(json, null, 4), 'utf8');
+                var attachment = new MessageAttachment(buffer, 'backup.json');
+
+                msg.channel.send(attachment);
+            } catch (err) {
+                return m.channel.send(newEmb(m).setColor(colors.error).setTitle("There was an error parsing your file ._."))
+            }
+        })
     }
 );
+
+function streamToString(stream) {
+    const chunks = []
+    return new Promise((resolve, reject) => {
+        stream.on('data', chunk => chunks.push(chunk))
+        stream.on('error', reject)
+        stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+    })
+}
