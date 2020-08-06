8dee20be3049456d2150cc51ca1b24d3c170cfff
Maf <dragoncatm@gmail.com>
2020-08-06 11:29:21 +0200

    Fixxed a Bug
     > Bot sending message twice

--- build/commands/show.js
+++ build/commands/show.js
@@ -16,9 +16,6 @@ module.exports = new classes_1.Command({
     bot_permissions: ['ADMINISTRATOR']
 }, async (msg, args) => {
     //Getting the file from the User
-    var emb = utilities_1.newEmb(msg).setColor(utilities_1.colors.info);
-    emb.setTitle("Please send me your JSON File uwu").setDescription("*Write* `cancel` *to abort*").setFooter("I will wait 30 Seconds");
-    await msg.channel.send(emb);
     utilities_1.getFile(msg, "Send me your JSON File uwu", 30, (json) => {
         //Converting to GuildStructure Object
         var structure = utilities_1.importGuild(json);
--- commands/show.ts
+++ commands/show.ts
@@ -19,10 +19,6 @@ module.exports = new Command({
 
     async (msg, args) => {
         //Getting the file from the User
-        var emb = newEmb(msg).setColor(colors.info);
-        emb.setTitle("Please send me your JSON File uwu").setDescription("*Write* `cancel` *to abort*").setFooter("I will wait 30 Seconds");
-        await msg.channel.send(emb)
-
         getFile(msg, "Send me your JSON File uwu", 30, (json) => {
             //Converting to GuildStructure Object
             var structure = importGuild(json);
