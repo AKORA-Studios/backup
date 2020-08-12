e2a23fd832bd1a4805ffa8cf76988c35917c8d7b
Maf <dragoncatm@gmail.com>
2020-08-12 12:31:09 +0200

    Added Fun Fact Command

---
+++ commands/fun_fact.ts
@@ -0,0 +1,39 @@
+import { newEmb, colors } from '../typescript/utilities';
+import * as bent from "bent";
+import { Command } from "../typescript/classes";
+
+
+module.exports = new Command({
+    name: 'Fun Fact',
+    syntax: 'fact',
+    args: false,
+    description: 'Sends you a fun fact uwu',
+    module_type: 'fun',
+    triggers: ['fact', 'funfact'],
+    user_permissions: ['SEND_MESSAGES'],
+    bot_permissions: ['SEND_MESSAGES']
+},
+
+    async (msg, args) => {
+
+        const getString = bent('string');
+
+        //Downloading the File
+        try {
+            var res = await getString("https://nekos.life/api/v2/fact")
+
+            //Parsing
+            try {
+                var json = JSON.parse(res);
+
+                msg.channel.send(newEmb(msg).setColor(colors.success).setDescription("*"+json.fact+"*").setTitle("Fun Fact:"))
+            } catch (err) {
+                console.log(err);
+                msg.channel.send(newEmb(msg).setColor(colors.error).setTitle("There was an error extracting the fact :0"));
+            }
+        } catch (err) {
+            console.log(err);
+            msg.channel.send(newEmb(msg).setColor(colors.error).setTitle("There was an error catching a funny fact >~>"));
+        }
+    }
+);
