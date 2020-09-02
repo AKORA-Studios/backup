fcacad57baa7da29e5c9dbbed84f94ab610a6b0e
Maf <dragoncatm@gmail.com>
2020-09-02 19:55:33 +0200

    Added restore

--- src/commands/backup_restore.ts
+++ src/commands/backup_restore.ts
@@ -1,6 +1,6 @@
 import { colors, confirmAction, getFile, importGuild, newEmb, rawEmb, emojis } from "../typescript/utilities";
 import { Command } from "../typescript/classes";
-import { Message, PermissionOverwrites } from "discord.js";
+import { Message, PermissionOverwrites, Role, TextChannel } from "discord.js";
 import { RoleStructure } from "../typescript/structures";
 
 module.exports = new Command({
@@ -27,7 +27,9 @@ module.exports = new Command({
                 .setDescription("If you're not **110%** sure this is the right backup use the `show` command to verify, or create a new one with the `save` command")
             );
 
-            confirmAction(message, "Please Confirm you want to load this Backup", async () => {
+            confirmAction(message, "Please Confirm you want to restore this Backup", async () => {
+                guild = await msg.guild.fetch();
+
                 var emb = newEmb(message).setColor(colors.success).setTitle("Loading Backup"),
                     text =
                         emojis.false + " Emojis\n"
@@ -54,6 +56,8 @@ module.exports = new Command({
 
                 if (struc.emojis) {//Else Not interable
                     for (let emoji of struc.emojis) {
+                        if (guild.emojis.resolve(emoji.id)) continue;//Already exists
+
                         await msg.guild.emojis.create(emoji.url, emoji.name, {
                             reason: reason
                         }).catch((e) => catchErr(msg, emoji.name, e));
@@ -75,16 +79,29 @@ module.exports = new Command({
                     var role = struc.roles.reverse()[i];
 
                     if (role.name !== "@everyone") {
-                        let r = await msg.guild.roles.create({
-                            data: {
-                                name: role.name,
-                                color: role.color,
-                                hoist: role.hoist,
-                                permissions: role.permissions,
-                                mentionable: role.mentionable
-                            }, reason: reason
-                        }).catch((e) => catchErr(msg, role.name, e));
-                        struc.roles.reverse()[i].loadedID = r["id"];
+                        if (guild.roles.resolve(role.id)) {
+                            let r = guild.roles.resolve(role.id);
+                            r.setColor(role.color, reason).catch(e => catchErr(msg, role.name, e));
+
+                            if (r.hoist !== role.hoist) r.setHoist(role.hoist, reason).catch(e => catchErr(msg, role.name, e));
+                            if (r.mentionable !== role.mentionable) r.setMentionable(role.mentionable, reason).catch(e => catchErr(msg, role.name, e));
+                            if (r.name !== role.name) r.setName(role.name, reason).catch(e => catchErr(msg, role.name, e));
+                            if (r.permissions.toArray() !== role.permissions) r.setName(role.name, reason).catch(e => catchErr(msg, role.name, e));
+                            if (r.position !== role.position) r.setPermissions(role.permissions).catch(e => catchErr(msg, role.name, e));
+
+                            struc.roles.reverse()[i].loadedID = role.id;
+                        } else {
+                            let r = await msg.guild.roles.create({
+                                data: {
+                                    name: role.name,
+                                    color: role.color,
+                                    hoist: role.hoist,
+                                    permissions: role.permissions,
+                                    mentionable: role.mentionable
+                                }, reason: reason
+                            }).catch((e) => catchErr(msg, role.name, e));
+                            struc.roles.reverse()[i].loadedID = r["id"];
+                        }
                     } else {
                         msg.guild.roles.resolve(msg.guild.id).setPermissions(role.permissions);
                     }
@@ -104,16 +121,31 @@ module.exports = new Command({
 
                 if (struc.channels) {//Else Not interable
                     //LOOSE - Channels
-                    for (let channel of struc.channels.filter(c => ["text"].includes(c.type))) {
-                        let c = await msg.guild.channels.create(channel.name, {
-                            permissionOverwrites: channel.permissionOverwrites.map((p) => mapPerms(p, struc.roles)),
-                            topic: channel.topic,
-                            type: channel.type,
-                            nsfw: channel.nsfw,
-                            position: struc.channels.indexOf(channel),
-                            reason: reason
-                        }).catch((e) => catchErr(msg, channel.name, e));
+                    const LOOSE = struc.channels.filter(c => ["text"].includes(c.type));
+                    for (let i = 0; i < LOOSE.length; i++) {
+                        let channel = LOOSE[i];
 
+                        if (guild.channels.resolve(channel.id)) {
+                            let c = guild.channels.resolve(channel.id) as TextChannel;
+
+                            if (c.name !== channel.name) c.setName(channel.name, reason).catch(e => catchErr(msg, channel.name, e));
+                            if (c.topic !== channel.topic) c.setTopic(channel.topic, reason).catch(e => catchErr(msg, channel.name, e));
+                            if (c.position !== i) c.setPosition(i).catch(e => catchErr(msg, channel.name, e));
+
+                            var overwrites = channel.permissionOverwrites.map((p) => mapPerms(p, struc.roles));
+                            if (c.permissionOverwrites.array() !== overwrites) c.overwritePermissions(overwrites).catch(e => catchErr(msg, role.name, e));
+
+                            c.setNSFW(channel.nsfw).catch(e => catchErr(msg, channel.name, e));
+                        } else {
+                            let c = await msg.guild.channels.create(channel.name, {
+                                permissionOverwrites: channel.permissionOverwrites.map((p) => mapPerms(p, struc.roles)),
+                                topic: channel.topic,
+                                type: channel.type,
+                                nsfw: channel.nsfw,
+                                position: struc.channels.indexOf(channel),
+                                reason: reason
+                            }).catch((e) => catchErr(msg, channel.name, e));
+                        }
                         //struc.channels[struc.channels.indexOf(channel)].loadedID = c["id"];
                     }
 
@@ -129,17 +161,34 @@ module.exports = new Command({
                             .catch((e) => catchErr(msg, category.name, e));
 
 
-                        for (let chan of category.childs) {
-                            let c = await msg.guild.channels.create(chan.name, {
-                                permissionOverwrites: chan.permissionOverwrites.map((p) => mapPerms(p, struc.roles)),
-                                topic: chan.topic,
-                                type: chan.type,
-                                nsfw: chan.nsfw,
-                                parent: category.loadedID,
-                                reason: reason
-                            })
-                                .then((c) => chan.loadedID = c["id"])
-                                .catch((e) => catchErr(msg, chan.name, e));
+
+                        for (let i = 0; i < category.childs.length; i++) {
+                            let chan = category.childs[i];
+
+                            if (guild.channels.resolve(chan.id)) {
+                                let c = guild.channels.resolve(chan.id) as TextChannel;
+
+                                if (c.name !== chan.name) c.setName(chan.name, reason).catch(e => catchErr(msg, chan.name, e));
+                                if (c.topic !== chan.topic) c.setTopic(chan.topic, reason).catch(e => catchErr(msg, chan.name, e));
+                                if (c.position !== i) c.setPosition(i).catch(e => catchErr(msg, chan.name, e));
+
+                                var overwrites = chan.permissionOverwrites.map((p) => mapPerms(p, struc.roles));
+                                if (c.permissionOverwrites.array() !== overwrites) c.overwritePermissions(overwrites).catch(e => catchErr(msg, role.name, e));
+
+                                c.setNSFW(chan.nsfw).catch(e => catchErr(msg, chan.name, e));
+                            } else {
+
+                                let c = await msg.guild.channels.create(chan.name, {
+                                    permissionOverwrites: chan.permissionOverwrites.map((p) => mapPerms(p, struc.roles)),
+                                    topic: chan.topic,
+                                    type: chan.type,
+                                    nsfw: chan.nsfw,
+                                    parent: category.loadedID,
+                                    reason: reason
+                                })
+                                    .then((c) => chan.loadedID = c["id"])
+                                    .catch((e) => catchErr(msg, chan.name, e));
+                            }
                         }
                     }
                 }
