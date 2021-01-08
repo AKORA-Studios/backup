2d3fa9a0b20ae5867ca7a9cfd73af83f00e4936a
Maf <dragoncatm@gmail.com>
2021-01-08 10:54:51 +0100

    Added command queue

--- src/commands/dev_shutdown.ts
+++ src/commands/dev_shutdown.ts
@@ -1,5 +1,5 @@
-import { newEmb, colors, confirmAction } from '../typescript/utilities';
-import { Command } from "../typescript/classes";
+import { newEmb, colors, confirmAction, rawEmb } from '../typescript/utilities';
+import { Bot, Command } from "../typescript/classes";
 import { Message } from 'discord.js';
 
 
@@ -14,12 +14,12 @@ module.exports = new Command({
     bot_permissions: []
 },
 
-    async (msg, args) => {
+    (msg, args, bot) => {
         if (args[0] && args[0].toLowerCase().includes('force')) {
-            smoothShutdown(msg);
+            smoothShutdown(msg, bot);
         } else {
             confirmAction(msg, 'Do you want to shutdown the bot?', () => {
-                smoothShutdown(msg);
+                smoothShutdown(msg, bot);
             }, () => {
 
             })
@@ -27,8 +27,14 @@ module.exports = new Command({
     }
 );
 
-const smoothShutdown = (msg: Message) => {
-    msg.client.user.setPresence({
+async function smoothShutdown(msg: Message, bot: Bot) {
+    if (bot.queue.length > 0) {
+        var m = await msg.channel.send(rawEmb().setColor(colors.warning).setTitle('Waiting for queue').setFooter(`There are ${bot.queue.length} commands in the queue`));
+        await Promise.all(bot.queue);
+        await m.edit(rawEmb().setColor(colors.success).setDescription('Shutting down...'));
+    }
+
+    await msg.client.user.setPresence({
         activity: {
             name: 'suicide...',
             type: 'PLAYING'
