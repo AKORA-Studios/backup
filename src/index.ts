a9cec90f8f776200545761f6505c67b16a9f0337
Maf <dragoncatm@gmail.com>
2021-01-31 21:42:38 +0100

    👈 Removed Autoquote

--- src/index.ts
+++ src/index.ts
@@ -72,45 +72,4 @@ async function guildCountUpdate(g: Guild, join: boolean) {
 client.on("guildCreate", g => guildCountUpdate(g, true));
 client.on("guildDelete", g => guildCountUpdate(g, false));
 
-client.on("message", async (msg) => {
-    if (msg.channel.type !== "text") return;
-    if (!(msg.channel instanceof TextChannel)) return;
-
-    var msg_link_regex = /https:\/\/(discord|discordapp)\.com\/channels\/([0-9]{18})\/([0-9]{18})\/([0-9]{18})/g,
-        id_regex = /([0-9]{18})/g,
-        emb = new MessageEmbed().setFooter(msg.author.tag, msg.author.displayAvatarURL());
-
-    var links = msg.content.match(msg_link_regex);
-    if (links === null) return;
-
-    for (let i = 0; i < links.length; i++) {
-        var link = links[i],
-            ids = link.match(id_regex);
-
-        if (ids === null || ids.length < 3) return;
-
-        try {
-            var guild = await client.guilds.fetch(ids[0]).catch();
-            if (!guild) return;
-
-            var channel = guild.channels.resolve(ids[1]) as TextChannel;
-            if (!channel || channel.type !== "text") return;
-
-            var message = await channel.messages.fetch(ids[2]).catch();
-            if (!message) return;
-
-            emb.setAuthor(message.author.tag, message.author.displayAvatarURL())
-            emb.setColor(colors.unimportant);
-            emb.setDescription(message.content);
-        } catch (e) {
-            //If something is wrong
-            emb.setTitle("Invalid");
-            emb.setColor(colors.error);
-            return;
-        }
-
-        msg.channel.send(emb).catch();
-    }
-});
-
 client.login(client.token);
